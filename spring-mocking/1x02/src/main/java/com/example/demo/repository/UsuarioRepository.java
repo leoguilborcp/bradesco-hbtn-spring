4765008030b1f7e6f5ba8511af3a694b58298dd9
leandro Ribeiro de Lima <leandro.r.lima@next.b.br>
2025-12-19 20:14:11 -0300

    Refactor UsuarioRepository to include findById and save methods

--- spring-mocking/1x02/src/main/java/com/example/demo/repository/UsuarioRepository.java
+++ spring-mocking/1x02/src/main/java/com/example/demo/repository/UsuarioRepository.java
@@ -6,8 +6,11 @@ import com.example.demo.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
+        Optional<Usuario> findById(Long id);
+        Usuario save(Usuario usuario);
 }
